59946363bfff2b79e2c48cd15f630447895eaf3f
Karanbairwaa12 <karanbairwaa@gmail.com>
2024-01-04 19:01:40 +0530

    add new arrivals product

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,7 +5,7 @@ import "./slick.css";
 import "./home.css";
 import { MdKeyboardArrowRight, MdKeyboardArrowLeft } from "react-icons/md";
 import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
-import { homeCorsoule } from "../../utils/constant";
+import { homeCorsoule, newArrivalsProduct, noOfProducts } from "../../utils/constant";
 import HomeCrosel from "../../components/crousel/HomeCrosel";
 
 const Home = () => {
@@ -46,22 +46,110 @@ const Home = () => {
   });
 
   return (
-    <div className="">
-      <div className="h-[110vh] overflow-hidden">
-        <Slider {...settings} ref={sliderRef}>
-          {homeCorsoule.map((item) => {
+    <div className="w-full">
+      {/* first */}
+      <div className="mb-[80px]">
+        <div className="h-[110vh] overflow-hidden">
+          <Slider {...settings} ref={sliderRef}>
+            {homeCorsoule.map((item) => {
+              return <HomeCrosel item={item} />;
+            })}
+          </Slider>
+        </div>
+        <button onClick={goToPrevSlide} className="arrayBttnleft">
+          <MdKeyboardArrowLeft className="arrow" />
+        </button>
+        <button onClick={goToNextSlide} className="arrayBttnright">
+          <MdKeyboardArrowRight className="arrow" />
+        </button>
+      </div>
+
+      {/* second */}
+      <div className="mb-[80px]">
+        <div className="p-[15px] mx-[15.5px] flex justify-around">
+          {noOfProducts.map((item) => {
             return (
-              <HomeCrosel item={item}/>
+              <div className="group w-[250px] itemxs flex flex-col justify-center text-center mx-[5px]">
+                <div className="itemxsImg w-[140px] mx-[auto] rounded-full overflow-hidden mb-[10px]">
+                  <img
+                    src={item.img}
+                    alt=""
+                    className="w-full group-hover:scale-[115%] transition-transform"
+                  />
+                </div>
+                <div>
+                  <h2 className="text-[20px]">{item.name}</h2>
+                </div>
+                <div>
+                  <p className="text-[14px] text-[gray]">
+                    {item.noOfProduct} Product
+                  </p>
+                </div>
+              </div>
             );
           })}
-        </Slider>
+        </div>
       </div>
-      <button onClick={goToPrevSlide} className="arrayBttnleft">
-        <MdKeyboardArrowLeft className="arrow" />
-      </button>
-      <button onClick={goToNextSlide} className="arrayBttnright">
-        <MdKeyboardArrowRight className="arrow" />
-      </button>
+
+      {/* third */}
+      <div className="w-full">
+        <div className="p-[15px] mx-[15.5px]">
+          <div className="flex flex-col text-center mb-[10px]">
+            <div>
+              <p className="mb-[10px] text-[16px] text-[blue]">
+                Hurry Up to Buy
+              </p>
+            </div>
+            <div>
+              <h1 className="mb-[10px] text-[38px] font-[500]">New Arrivals</h1>
+            </div>
+            <div>
+              <p className="mb-[10px] text-[16px] text-[gray]">
+                How can you evaluate content without design
+              </p>
+            </div>
+            <div className="flex justify-center mt-[20px]">
+              <div className="mr-[30px] ml-[5px]">
+                <h2 className="caseListing">CASES</h2>
+              </div>
+              <div className="mr-[30px] ml-[5px]">
+                <h2 className="caseListing">STRAPS</h2>
+              </div>
+              <div className="mr-[30px] ml-[5px]  ">
+                <h2 className="caseListing">MAGSAFE</h2>
+              </div>
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="px-[15px]">
+              <div className="grid grid-rows-2 grid-cols-5 gap-[30px]">
+                {newArrivalsProduct.map((item) => {
+                  return (
+                    <div className="flex flex-col text-center">
+                      <div className="overflow-hidden rounded-[30px] object-cover border mb-[10px]">
+                        <img src={item.img} />
+                      </div>
+                      <div>
+                        <h3 className="text-[12px] font-[500] mb-[10px]">
+                          {item.productName}
+                        </h3>
+                      </div>
+                      <div>
+                        <p className="text-[12px] text-[gray] mb-[10px]">
+                          {item.productDetail}
+                        </p>
+                      </div>
+                      <div className="text-[blue]  mb-[10px]">
+                        <p>${item.price}</p>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -15,3 +15,99 @@ export const homeCorsoule = [
         about:"A client that’s unhappy for a reason is a problem, a client that’s unhappy though required he or her can’t quite put a finger."
     }
 ]
+
+export const noOfProducts = [
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/08/wood-accessories-category-1-132.jpg.webp",
+        name:"Cases",
+        noOfProduct:51,
+    },    
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/08/wood-accessories-category-2.jpg.webp",
+        name:"MagSafe",
+        noOfProduct:15,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/08/wood-accessories-category-3.jpg.webp",
+        name:"Cables",
+        noOfProduct:18,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/08/wood-accessories-category-4.jpg.webp",
+        name:"Charger",
+        noOfProduct:12,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/08/wood-accessories-category-5.jpg.webp",
+        name:"Straps",
+        noOfProduct:38,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/09/wood-accessories-category-6-1.jpg.webp",
+        name:"Power Banks",
+        noOfProduct:18,
+    }
+] 
+
+export const newArrivalsProduct = [
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/09/blue-case-back-430x491.jpg",
+        productName:"iPhone 12 Pro Moment Case – Blue",
+        productDetail:"IPhone 12 pro",
+        price:149.00,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/08/w-accessories-product-iphone-xr-10-1-430x491.jpg",
+        productName:"Full Aquarelle iPhone XR",
+        productDetail:"IPhone XR",
+        price:169.00,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/09/green-case-back-430x491.jpg",
+        productName:"iPhone 12 Pro Moment Case – Olive",
+        productDetail:"IPhone 12 pro",
+        price:149.00,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/08/w-accessories-product-iphone-12-violet-1-430x491.jpg",
+        productName:"Leather Case iPhone 12 Deep Violet",
+        productDetail:"IPhone 12",
+        price:230.00,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/09/w-accessories-product-iphone-13-3-430x491.jpg",
+        productName:"iPhone 13 Case Luxe – Dusty Pink",
+        productDetail:"IPhone 13",
+        price:149.00,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/09/w-accessories-product-iphone-13-6-430x491.jpg",
+        productName:"iPhone 13 Case Max – Black",
+        productDetail:"IPhone 13",
+        price:159.00,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/09/w-accessories-product-iphone-13-2-430x491.jpg",
+        productName:"iPhone 13 Case With MagSafe – Red Rose",
+        productDetail:"IPhone 13",
+        price:199.00,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/08/w-accessories-product-link-3-1-430x491.jpg",
+        productName:"Epik Silicone Case Full – Lavender",
+        productDetail:"IPhone 12",
+        price:99.00,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/08/w-accessories-product-12-pro-4-430x491.jpg",
+        productName:"iPhone 12 Pro Max Silicone – Black/White",
+        productDetail:"IPhone 12 pro",
+        price:99.00,
+    },
+    {
+        img:"https://woodmartcdn-cec2.kxcdn.com/accessories/wp-content/uploads/sites/7/2022/08/w-accessories-product-iphone-11-pro-7-430x491.jpg",
+        productName:"Leather Case iPhone 11 Pro",
+        productDetail:"IPhone 11 pro",
+        price:230.00,
+    }
+]
